272852fe953d7d264b2af675c834a4590f964d3a
liuzhaowei <liuzhaowei@cestc.cn>
2022-06-25 16:46:18 +0800

    leetcode88

---
+++ algorithm/lettcode88_test.go
@@ -0,0 +1,22 @@
+package algorithm
+
+import "testing"
+
+func TestMergeReverseDoublePoint(t *testing.T) {
+
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+	MergeDoublePoint(nums1, m, nums2, n)
+
+}
+func TestMergeDoublePoint(t *testing.T) {
+
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+	MergeDoublePoint(nums1, m, nums2, n)
+
+}
