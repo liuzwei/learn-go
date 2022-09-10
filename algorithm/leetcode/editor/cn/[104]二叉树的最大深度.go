f03bc2a7d545d5586e687c41c9aaaa2e641ec386
Liuzhaowei <liuzhaowei.cool@163.com>
2022-09-10 13:55:08 +0800

    linked list traverse

---
+++ algorithm/leetcode/editor/cn/[104]二叉树的最大深度.go
@@ -0,0 +1,30 @@
+package cn
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftMax := maxDepth(root.Left)
+	rightMax:=maxDepth(root.Right)
+	if leftMax > rightMax {
+		return leftMax + 1
+	}else {
+		return rightMax + 1
+	}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
