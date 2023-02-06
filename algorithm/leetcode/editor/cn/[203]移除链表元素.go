cced7eb2f9539ed0916c11dd96b907fefa774a93
Liuzhaowei <liuzhaowei.cool@163.com>
2023-02-06 10:42:55 +0800

    迭代删除链表节点元素

--- algorithm/leetcode/editor/cn/[203]移除链表元素.go
+++ algorithm/leetcode/editor/cn/[203]移除链表元素.go
@@ -9,16 +9,22 @@ package cn
  * }
  */
 func removeElements(head *ListNode, val int) *ListNode {
-	if head == nil {
-		return head
+	// 迭代法删除节点
+
+	// 创建一个虚拟节点
+	dummy := &ListNode{Val: 0, Next: head}
+	temp := dummy
+	for temp.Next != nil {
+		// 判断节点是否等于val
+		if temp.Next.Val == val {
+			// 说明该节点需要删除
+			temp.Next = temp.Next.Next
+		}else {
+			// 该节点不用删除，移动到下一节点
+			temp = temp.Next
+		}
 	}
-	// 递归删除节点,从后往前删
-	head.Next = removeElements(head.Next, val)
-	if head.Val == val {
-		return head.Next
-	} else {
-		return head
-	}
+	return dummy.Next
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
