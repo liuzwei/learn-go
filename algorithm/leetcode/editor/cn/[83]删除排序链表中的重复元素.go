1f89e736f947248f5875cfed48080050662e930a
Liuzhaowei <liuzhaowei.cool@163.com>
2023-02-09 13:38:54 +0800

    删除链表重复元素

---
+++ algorithm/leetcode/editor/cn/[83]删除排序链表中的重复元素.go
@@ -0,0 +1,24 @@
+package cn
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{Val: 0, Next: head}
+	tail := dummy.Next
+	for tail != nil{
+		if tail.Next != nil && tail.Val == tail.Next.Val {
+			tail.Next = tail.Next.Next
+		}else {
+			tail = tail.Next
+		}
+	}
+	return dummy.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
