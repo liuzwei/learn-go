184b0e1b57c534c5647de0a7ec68e44bb84420ff
liuzhaowei <liuzhaowei.cool@163.com>
2023-11-27 15:48:00 +0800

    压缩字符串

---
+++ algorithm/leetcode/editor/cn/leetcode_面试题 01.06_字符串压缩_test.go
@@ -0,0 +1,23 @@
+package cn
+
+import "testing"
+
+func Test_compressString(t *testing.T) {
+	type args struct {
+		S string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "测试用例1", args: args{S: "aabcccccaa"}, want: "a2b1c5a2"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compressString(tt.args.S); got != tt.want {
+				t.Errorf("compressString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
